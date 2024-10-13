be7674f8d9f2e70b900df166031deec67526fe30
Vinícius Santos <vinisantoosk@gmail.com>
2024-10-13 17:51:35 -0300

    feat: aplicação completa

---
+++ src/components/Cabecalho.vue
@@ -0,0 +1,14 @@
+<script setup>
+
+</script>
+
+<template>
+    <header class="p-5 mb-4 mt-4 bg-light rounded-3">
+        <h1 class="mb-5">Calculadora Aritmética</h1>
+        <p>A calculadora realizará o cálculo automaticamente sempre que houver alguma alteração nos valores dos campos
+            de entrada ou na seleção da operação. Não será necessário um botão específico para iniciar o cálculo.</p>
+
+    </header>
+</template>
+
+<style scoped></style>
---
+++ src/components/Calculadora.vue
@@ -0,0 +1,29 @@
+<script setup>
+const props = defineProps(['selecionaNumeroA','editaNumA', 'selecionaOperacao','selecionaNumeroB','editaNumB','resultado'])
+
+
+</script>
+
+<template>
+    <section class="calc">
+        <div class="row justify-content-between">
+            <input :value="props.selecionaNumeroA" @change="props.editaNumA" class="col-md-2" type="number" name="" id="">
+            <select @change="props.selecionaOperacao" class="col-md-2 ">
+                <option value="+">+</option>
+                <option value="-">-</option>
+                <option value="*">*</option>
+                <option value="/">/</option>
+            </select>
+            <input :value="props.selecionaNumeroB" @change="props.editaNumB" class="col-md-2" type="number" name="" id="">
+        </div>
+        <h3 class="mt-5">Resultado</h3>
+        <div class="result">
+             
+            <h4>{{ props.resultado }}</h4>
+        </div>
+    </section>
+
+
+</template>
+
+<style scoped></style>
